335a3a90b93fc25c144580fae2d70873a24859b2
Teodora Djeri <teodoradjeri@gmail.com>
2026-01-16 22:06:07 +0100

    Transaction resurs definisan

---
+++ urban-properties-api/app/Http/Resources/TransactionResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TransactionResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'offer_id' => $this->offer_id,
+            'property_id' => $this->property_id,
+            'buyer_id' => $this->buyer_id,
+            'sales_agent_id' => $this->sales_agent_id,
+            'final_price' => $this->final_price,
+            'signed_at' => $this->signed_at,
+            'payment_status' => $this->payment_status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+
+            'offer' => $this->whenLoaded('offer', function () {
+                return (new OfferResource($this->offer))->resolve();
+            }),
+            'buyer' => $this->whenLoaded('buyer', function () {
+                return (new UserResource($this->buyer))->resolve();
+            }),
+            'sales_agent' => $this->whenLoaded('salesAgent', function () {
+                return (new UserResource($this->salesAgent))->resolve();
+            }),
+        ];
+    }
+}
